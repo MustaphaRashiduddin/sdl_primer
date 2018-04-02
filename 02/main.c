606c0eeee06e7f595a8859a04c731b16190eaebd
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2018-04-02 10:27:42 +0100

    initial commit

---
+++ 02/main.c
@@ -0,0 +1,71 @@
+#include <SDL2/SDL.h>
+typedef enum { false, true } bool;
+
+bool init();
+bool load_media();
+void close();
+
+SDL_Window *window = 0;
+SDL_Surface *screen_surface = 0;
+SDL_Surface *hello_world = 0;
+
+bool init();
+bool load_media();
+void close();
+
+int main(int argc, char **argv)
+{
+        if (!init()) {
+                printf("Failed to initialize!\n");
+        } else {
+                if (!load_media()) {
+                        printf("Failed to load media!\n");
+                } else {
+                        SDL_BlitSurface(hello_world, 0, screen_surface, 0);
+                        SDL_UpdateWindowSurface(window);
+                        SDL_Delay(2000);
+                }
+        }
+        close();
+}
+
+bool init()
+{
+        bool success = true;
+
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+                success = false;
+        } else {
+                window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+                if (window == 0) {
+                        printf("Window could not be created!! SDL_Error: %s\n", SDL_GetError());
+                        success = false;
+                } else {
+                        screen_surface = SDL_GetWindowSurface(window);
+                }
+        }
+        return success;
+}
+
+bool load_media()
+{
+        bool success = true;
+        hello_world = SDL_LoadBMP("hello_world.bmp");
+        if (hello_world == 0) {
+                printf("Unable to load image %s! SDL_Error: %s\n", "hello_world.bmp", SDL_GetError());
+                success = false;
+        }
+        return success;
+}
+
+void close()
+{
+        SDL_FreeSurface(hello_world);
+        hello_world = 0;
+
+        SDL_DestroyWindow(window);
+        window = 0;
+
+        SDL_Quit();
+}
